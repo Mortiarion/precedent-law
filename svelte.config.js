2ea30c142ea4f208e8d01ea9a42a268a965512fe
Mortiarion <andre-endju@ukr.net>
2026-03-06 18:38:23 +0200

    1. vercel

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-auto';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -7,8 +7,8 @@ const config = {
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
 		adapter: adapter({
-			pages: 'build',
-			assets: 'build'
+			// pages: 'build',
+			// assets: 'build'
 		})
 	}
 };
